c627ecd497915f77071ac60bbce77f0ba985d26d
kimnorth <benignhumour@gmail.com>
2017-02-15 15:26:19 +0000

    Define artist class

--- models/artists.rb
+++ models/artists.rb
@@ -1,13 +1,21 @@
+require_relative('../sql_runner.rb')
+
 class Artist
 
   def initialize(options)
     @name = options["name"]
-    @id = options["id"] if options["id"]
+    @artist_id = options["artist_id"] if options["artist_id"]
   end
 
   def save()
 
-    
+    sql = "INSERT INTO artists 
+    (name)
+    VALUES
+    ('#{@name}') RETURNING artist_id;"
+
+    results = SqlRunner.run(sql)
+    @artist_id = results.first['artist_id'].to_i
 
   end
 
