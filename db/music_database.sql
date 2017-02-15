31b179501292aa8465792d07c2f8ec6b1d1c3561
kimnorth <benignhumour@gmail.com>
2017-02-15 15:11:35 +0000

    Database file complete

---
+++ db/music_database.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+  artist_id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+
+CREATE TABLE albums (
+  album_id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  artist INT4 REFERENCES artists(artist_id) -- Foreign Key
+);
